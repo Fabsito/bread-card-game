3db8b98ae3eb901952ece20489241266499d95c4
kirellos11 <kirellos.samir.sary@gmail.com>
2026-03-24 13:00:32 +0200

    error fix

--- scripts/player_hand.gd
+++ scripts/player_hand.gd
@@ -1,17 +1,19 @@
 extends Node2D
+@onready var main: Node2D = $".."
 const HAND_COUNT: int = 5
+
 const Card = preload("uid://b0bddfu4w10nu")
-const CARD_WIDTH : int= 100
+const CARD_WIDTH : int= 40
 var hand_y_position
 @onready var card_manager: Node2D = $"../card_manager"
 var player_hand :Array = []
 var center_screen_x
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var viewport_size = get_viewport().size
-	center_screen_x = card_manager.to_local(Vector2(viewport_size.x / 2, 0)).x
+	var viewport_size = get_viewport_rect().size
+	center_screen_x =viewport_size.x /2
+	print(center_screen_x)
 	hand_y_position = get_viewport().size.y - 300
-	print(hand_y_position)
 	for i in HAND_COUNT:
 		var new_card = Card.instantiate()
 		card_manager.add_child(new_card)
@@ -20,10 +22,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func animate_card_to_position(card , new_position):
 	var tween := get_tree().create_tween()
-	tween.tween_property(card,"position",new_position,0.1)
+	tween.tween_property(card,"global_position",new_position,0.1)
 func  add_card_to_hand(card):
 	player_hand.insert(0,card)
 	update_hand_positions()
+	print(player_hand)
 func update_hand_positions():
 	for i in range(player_hand.size()):
 		var new_pos = calculate_card_position(i)
@@ -32,5 +35,5 @@ func update_hand_positions():
 		
 func calculate_card_position(index):
 	var total_width = (player_hand.size() - 1) * float(CARD_WIDTH)
-	var x_offset = center_screen_x - total_width / 2.0 + index * float(CARD_WIDTH)
+	var x_offset = center_screen_x + index * CARD_WIDTH - total_width / 2
 	return Vector2(x_offset, hand_y_position)
